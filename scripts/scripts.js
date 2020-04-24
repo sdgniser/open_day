fdcb5212982c3c387826a2ed13ee16a6a082d834
Jyotirmaya Shivottam <31299799+JeS24@users.noreply.github.com>
2020-04-24 10:49:28 +0530

    Added gallery

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+// window.onload = clear();
+// function clear() {
+//     if ((document.body.textContent || document.body.innerText).indexOf('Ninja Slider trial version') > -1)
+//         document.body.innerText = document.body.innerText.replace('Ninja Slider trial version', 'ORANGE D JUS');
+//     if ((document.body.textContent || document.body.innerText).indexOf('Thumbnail Slider trial version') > -1)
+//         document.body.innerText = document.body.innerText.replace('Thumbnail Slider trial version', '');
+// }
